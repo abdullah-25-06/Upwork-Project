b07110c7206ae34cd75ba9bdc386d2eae3c83daf
charindu1993 <166448344+charindu1993@users.noreply.github.com>
2024-04-15 10:40:40 -0700

    Update key.js

--- jwt/key.js
+++ jwt/key.js
@@ -1,4 +1,8 @@
+// Import the crypto module
 const crypto = require("crypto");
 
+// Generate a random string of hexadecimal characters
 const jtis = crypto.randomBytes(64).toString("hex");
-console.log(jtis)
+
+// Output the generated random string
+console.log(jtis);
