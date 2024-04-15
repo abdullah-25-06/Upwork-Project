213915d1acd9dc1e9765a0023c0d07e4a9f9bc57
charindu1993 <166448344+charindu1993@users.noreply.github.com>
2024-04-15 10:42:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,23 @@ const cors = require("cors");
 const dotenv = require("dotenv").config();
 const { connectdb } = require("./DB/connect");
 
+// Enable CORS
 app.use(cors());
+
+// Parse JSON requests with a limit of 10mb
 app.use(express.json({ limit: "10mb" }));
+
+// Serve static files from the 'uploads' directory
 app.use("/uploads", express.static("uploads"));
+
+// Route requests to the userRouter
 app.use("/", userRouter);
 
+// Start the server and connect to the database
 const start = async () => {
   try {
     app.listen(8000, () =>
-      console.log("server is listening on http://localhost:8000")
+      console.log("Server is listening on http://localhost:8000")
     );
     await connectdb();
   } catch (error) {
