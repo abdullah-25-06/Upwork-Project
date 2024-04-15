fa7f2ccdd45dd285613cfcc23d09f7c346cec100
charindu1993 <166448344+charindu1993@users.noreply.github.com>
2024-04-15 10:39:47 -0700

    Update err.js

--- err/err.js
+++ err/err.js
@@ -1,8 +1,12 @@
+// Define a custom error class
 class CustomErrorApi extends Error {
-    constructor(message, statusCode) {
-      super(message);
-      this.statusCode = statusCode;
-    }
+  constructor(message, statusCode) {
+    // Call the Error constructor with the provided message
+    super(message);
+    // Assign the status code to the instance property
+    this.statusCode = statusCode;
   }
-  
+}
+
+// Export the CustomErrorApi class for use in other modules
 module.exports = CustomErrorApi;
